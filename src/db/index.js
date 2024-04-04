50b2abdb261ffb6efc160e7f0380f64a31ed8d0b
Gautam Sharma <gautam2586272@gmail.com>
2024-04-04 19:25:29 +0530

    utils code

--- src/db/index.js
+++ src/db/index.js
@@ -10,7 +10,7 @@ const connectDB = async () => {
       `\n mongoDB connected ! DB HOST ${connectionInstance.connection.host}`
     );
   } catch (error) {
-    console.log("MongDB connection failed", error);
+    console.log("MongoDB connection failed", error);
     process.exit(1);
   }
 };
--- src/index.js
+++ src/index.js
@@ -8,7 +8,15 @@ dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server listening at port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection failed ! ", err);
+  });
 
 /*
 import { express } from "express";

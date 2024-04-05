ad4db06fe5b652100e0b431a58a29b19e6830166
Gautam Sharma <gautam2586272@gmail.com>
2024-04-05 17:13:55 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -16,4 +16,11 @@ app.use(express.urlencoded({ extended: true, limit: "10kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+// routes
+// import
+import userRouter from "./routes/user.routes.js";
+// declaration
+app.use("/api/v1/users", userRouter);
+// http://locahost:8000/api/v1/users/login
+
 export { app };

8a725dba76438fa454a189f7f283c6a998da3ef6
Gautam Sharma <gautam2586272@gmail.com>
2024-04-10 13:26:44 +0530

    add middleware and login logout controllers

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,27 +1,23 @@
 class ApiError extends Error {
   constructor(
-    statusCode, // Status code of the API response
-    message = "Something went wrong", // Default error message if not provided
-    errors = [], // Optional array to hold additional error details
-    stack = "" // Optional stack trace
+    statusCode,
+    message = "Something went wrong",
+    errors = [],
+    stack = ""
   ) {
-    super(message); // Call the constructor of the Error class with the provided message
+    super(message);
+    this.statusCode = statusCode;
+    this.data = null;
+    this.message = message;
+    this.success = false;
+    this.errors = errors;
 
-    // Initialize properties specific to ApiError instance
-    this.statusCode = statusCode; // Assign provided statusCode to this instance's statusCode property
-    this.data = null; // Initialize data property to null
-    this.message = message; // Assign provided message to this instance's message property
-    this.success = false; // Assign false to success property
-    this.errors = errors; // Assign provided errors array to this instance's errors property
-
-    // Conditionally set stack trace
     if (stack) {
-      this.stack = stack; // If stack is provided, assign it to this instance's stack property
+      this.stack = stack;
     } else {
-      // If stack is not provided, capture the stack trace
       Error.captureStackTrace(this, this.constructor);
     }
   }
 }
 
-export { ApiError }; // Export the ApiError class to make it accessible from other modules
+export { ApiError };
